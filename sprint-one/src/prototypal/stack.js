99d0e0b1cc8ebe4a5bf90aa6f96883a2ec7f3716
ericdo <ericdo.617@gmail.com>
2019-04-10 16:17:50 -0700

    Solved prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.index = 0;
+  obj.storage = {};
+
+  return obj;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(str) {
+  this.storage[this.index] = str;
+  this.index++;
+}
 
+stackMethods.pop = function() {
+  try {
+    if (this.size() === 0) {
+      throw 'Can\'t pop an empty stack';
+    } else {
+      var temp = this.storage[this.index - 1];
+      delete this.storage[this.index - 1];
+      this.index--;
+      return temp;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+stackMethods.size = function() {
+  return Object.keys(this.storage).length;
+}
