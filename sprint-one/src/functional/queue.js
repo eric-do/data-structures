7ef1dc64a2235b82f929031738190c1d8cdfc8ce
ericdo <ericdo.617@gmail.com>
2019-04-10 12:45:26 -0700

    Solved functional for stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,7 @@
 var Queue = function() {
   var someInstance = {};
+  var index = 0;
+  var back = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +9,27 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[index] = value;
+    index++;
   };
 
   someInstance.dequeue = function() {
+    try {
+      if (someInstance.size() === 0) {
+        throw "Cannot dequeue an empty queue";
+      } else {
+        var temp = storage[back];
+        delete storage[back];
+        back++; 
+        return temp;
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,33 @@
 var Stack = function() {
   var someInstance = {};
+  var index = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[index] = value;
+    index++;
   };
 
   someInstance.pop = function() {
+    try {
+      if (someInstance.size() === 0 || someInstance.size() === undefined) {
+        throw 'Cannot pop an empty stack';
+      } else {
+        var temp = storage[index - 1];
+        delete storage[index - 1];
+        index--;
+        return temp;
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
