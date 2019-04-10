9278a59a478d6df74c3c9d64eb61064e642996b4
ericdo <ericdo.617@gmail.com>
2019-04-10 15:07:10 -0700

    Solved functiona-shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,42 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = {
+    index: 0,
+    storage: {}
+  };
+
+  extend(obj, stackMethods);
+
+  return obj;
 };
 
 var stackMethods = {};
 
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
 
+stackMethods.push = function(str) {
+  this.storage[this.index] = str;
+  this.index++;
+};
+
+stackMethods.pop = function() {
+  try {
+    if (this.size() === 0) {
+      throw 'Cannot pop an empty stack';
+    } else {
+      var temp = this.storage[this.index - 1];
+      delete this.storage[this.index - 1];
+      this.index--;
+      return temp;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+stackMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
